80dd812ebf48ea928177463439b8db10f3066c48
Hendi Santika <hendisantika@gmail.com>
2017-06-12 10:52:39 +0700

    #4 - Added all codes

---
+++ src/main/java/com/hendisantika/google/oauth/GoogleOauthSpringBootSecurityAngularjsApplication.java
@@ -0,0 +1,18 @@
+package com.hendisantika.google.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+/**
+ * @author Sarath Muraleedharan
+ *
+ */
+@SpringBootApplication
+@EnableOAuth2Sso
+public class GoogleOauthSpringBootSecurityAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleOauthSpringBootSecurityAngularjsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hendisantika/google/oauth/web/UserRestController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.google.oauth.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * Rest controller for getting logged user details
+ * 
+ * @author Sarath Muraleedharan
+ *
+ */
+@RestController
+public class UserRestController {
+	@RequestMapping("/user")
+	public Principal sayHello(Principal principal) {
+		return principal;
+	}
+
+}
